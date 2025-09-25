3905b84efeafbe76b8ea36113123b20d0e34fc9d
tusharpaik123 <tusharpaik543@gmail.com>
2025-09-26 01:24:30 +0530

    chore: test setup implemented for frontend

---
+++ client/.jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+};
